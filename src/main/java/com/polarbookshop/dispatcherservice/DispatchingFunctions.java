d51f056c2fec75c90628861043462aa1037ef911
Jesus Ciriano <jciriano@nalandaglobal.com>
2022-08-04 14:02:28 +0200

    Funcion Label (reactive)

--- src/main/java/com/polarbookshop/dispatcherservice/DispatchingFunctions.java
+++ src/main/java/com/polarbookshop/dispatcherservice/DispatchingFunctions.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
@@ -18,5 +19,12 @@ public class DispatchingFunctions {
             logger.info("The order with id {} is packed.", orderAcceptedMessage.orderId());
             return orderAcceptedMessage.orderId();
         };
+    }
+    @Bean
+    public Function<Flux<Long>, Flux<OrderDispatchedMessage>> label(){
+        return orderFlux -> orderFlux.map(orderId -> {
+            logger.info("The order with id {} is labeled.", orderId);
+            return new OrderDispatchedMessage(orderId);
+        });
     }
 }
